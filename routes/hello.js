ad9b4675026621880df9094026898a13cdd3316a
stsutsumi-dl <arockbilly223@gmail.com>
2024-11-11 21:17:38 +0900

    5th course

--- routes/hello.js
+++ routes/hello.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', funcion(req, res, next) {
-    res.render('hello', { title: 'Hello Express'});
+router.get('/', function(req, res, next) {
+    res.render('hello', { title: 'Hello Express' });
 });
 
 module.exports = router;
